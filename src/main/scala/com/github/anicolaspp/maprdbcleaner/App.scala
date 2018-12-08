67a721df0b1cda6cf25729b5f0edb88c2f9647c9
Nicolas A Perez <anicolaspp@gmail.com>
2018-12-08 11:46:35 -0500

    Refactoring to use monadic structures

--- src/main/scala/com/github/anicolaspp/maprdbcleaner/App.scala
+++ src/main/scala/com/github/anicolaspp/maprdbcleaner/App.scala
@@ -1,41 +1,21 @@
 package com.github.anicolaspp.maprdbcleaner
 
 import com.github.anicolaspp.maprdbcleaner.conf.Configuration
-import org.ojai.store.{Connection, DocumentStore, DriverManager}
+import com.github.anicolaspp.maprdbcleaner.db.Db
+import com.github.anicolaspp.maprdbcleaner.query.QueryBuilder
+import org.ojai.store.{Connection, DriverManager}
 
 object App {
 
-  def main(args: Array[String]): Unit = {
+  implicit lazy val connection: Connection = DriverManager.getConnection("ojai:mapr:")
+
+  def main(args: Array[String]): Unit =
     Configuration
       .parse(args)
-      .foreach { config =>
-
-        implicit val connection: Connection = getConnection()
-
-        val query = buildQuery(config)
-
-        val documentStore: DocumentStore = connection.getStore(config.tableName)
-
-        val documents = documentStore.find(query)
-
-        documentStore.delete(documents, config.id)
-
-        documentStore.close()
-
-        println(s"Done deleting documents for query: ${query}")
+      .map(QueryBuilder.buildFrom)
+      .map(Db.run)
+      .foreach {
+        case Left(error) => println(error)
+        case Right(msg) => println(msg)
       }
-  }
-
-  private def getConnection() = DriverManager.getConnection("ojai:mapr:")
-
-  private def buildQuery(conf: Configuration)(implicit connection: Connection) =
-    if (conf.allDocuments) allDocumentsQuery() else someDocumentsQuery(conf.query)
-
-  private def allDocumentsQuery()(implicit connection: Connection) = connection.newQuery().build()
-
-  private def someDocumentsQuery(query: String)(implicit connection: Connection) = connection.newQuery(query).build()
 }
-
-
-
-
