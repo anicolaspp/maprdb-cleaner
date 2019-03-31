9446e0fb2de691c4377008bce293a9f7d7a7df26
Nicolas A Perez <anicolaspp@gmail.com>
2019-03-30 22:58:26 -0400

    release

--- maprdb-cleaner.rb
+++ maprdb-cleaner.rb
@@ -1,7 +1,7 @@
 class MaprdbCleaner < Formula
   desc "A small tool to clean MapR-DB based on queries"
   homepage "https://github.com/anicolaspp/maprdb-cleaner"
-  url "https://github.com/anicolaspp/maprdb-cleaner/releases/download/1.0.1/maprcls.tar"
+  url "https://github.com/anicolaspp/maprdb-cleaner/releases/download/1.0.1/maprdbcls.tar"
   sha256 "fc778d7b3ddb756af7d485c5e59dc4d62e129f57c98188f705dd1050eb880824"
   depends_on :java => "1.8+"
 
